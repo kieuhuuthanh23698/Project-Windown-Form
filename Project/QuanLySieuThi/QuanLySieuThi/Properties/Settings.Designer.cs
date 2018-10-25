90df93b0449f0bd8cc4279e73144136d3a0f3b2c
Kiều Hữu Thành <34344168+kieuhuuthanh23698@users.noreply.github.com>
2018-10-25 19:00:54 +0700

    tạo cryston report

--- Project/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
+++ Project/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuanLySieuThi.Properties
-{
-
-
+namespace QuanLySieuThi.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-CQ1BQQH5\\SQLEXPRESS;Initial Catalog=QL_SIEUTHI;Persist Securit" +
+            "y Info=True;User ID=sa;Password=23698")]
+        public string QL_SIEUTHIConnectionString {
+            get {
+                return ((string)(this["QL_SIEUTHIConnectionString"]));
             }
         }
     }
--- Project/QuanLySieuThi/QuanLySieuThi/frmReportHoaDon.cs
+++ Project/QuanLySieuThi/QuanLySieuThi/frmReportHoaDon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -10,16 +11,25 @@ namespace QuanLySieuThi
 {
     public partial class frmReportHoaDon : Form
     {
-        public frmReportHoaDon()
+        KetNoiDuLieu link;
+        string maHoaDon;
+        
+        public frmReportHoaDon(KetNoiDuLieu link,string maHoaDon)
         {
+            this.link = link;
+            this.maHoaDon = maHoaDon;
             InitializeComponent();
+            crystalReportViewer1.RefreshReport();
+
         }
 
         private void btn_Show_Click(object sender, EventArgs e)
         {
             Phieubaogia pbg = new Phieubaogia();
+            string chuoiQuery = "select * from ChiTietHoaDon, HoaDon where HoaDon.MaHoaDon = '" + maHoaDon + "' and ChiTietHoaDon.MaHoaDon = HoaDon.MaHoaDon";
+            DataTable dt = this.link.comManTable(chuoiQuery,"ChiTietHoaDon").Tables["ChiTietHoaDon"];
+            pbg.SetDataSource(dt);
             crystalReportViewer1.ReportSource = pbg;
-            pbg.SetDatabaseLogon("sa", "sa2012", "GMK1KUGNFEEWJKV\\SQLEXPRESS", "QL_SIEUTHI");
             crystalReportViewer1.Refresh();
         }
 
