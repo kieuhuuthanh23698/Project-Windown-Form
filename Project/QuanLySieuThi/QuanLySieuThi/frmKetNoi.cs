8abaa64710cc723a490107f15895eaae96f793f9
lethingochien <lethingochiencntp@gmail.com>
2018-10-26 23:35:35 +0700

    thanks

--- Project/QuanLySieuThi/QuanLySieuThi/frmKetNoi.cs
+++ Project/QuanLySieuThi/QuanLySieuThi/frmKetNoi.cs
@@ -27,12 +27,12 @@ namespace QuanLySieuThi
                 if (link.Connec() == true)
                 {
                     //đưa kết nối vào form login
-                    //frmLogin frmlogin = new frmLogin(link);
-                    //this.Hide();
-                    //frmlogin.Show();
-                    frmReportHoaDon frm = new frmReportHoaDon(link, "HOADON_1");
+                    frmLogin frmlogin = new frmLogin(link);
                     this.Hide();
-                    frm.Show();
+                    frmlogin.Show();
+                    //frmReportHoaDon frm = new frmReportHoaDon(link, "HOADON_1");
+                    //this.Hide();
+                    //frm.Show();
                 }
                 else
                 {
